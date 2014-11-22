6071c06d078ccfdc9c0cca0e736774556ea7dcd4
Andy Brown <pursuingpareto@gmail.com>
2014-11-22 14:27:23 -0800

    add 'clear' and change state toggling

--- script.js
+++ script.js
@@ -49,7 +49,6 @@ $('document').ready(function(){
 			return $(this.diceContainer).children()
 		},
 
-
 		initialize : function() {
 			//this.diceContainer.empty();
 			var die;
@@ -65,8 +64,9 @@ $('document').ready(function(){
 			var die;
 			for (var i=0; i<octopus.getAllDice().length; i++){
 				die = octopus.getDie(i);
-				$(this.diceContainer)[i].attr('class', 'die die-'+die.state);
-				$(this.diceContainer)[i].text(die.val);
+				console.log(die.id);
+				$('#die-'+die.id).attr('class', 'die die-'+die.state);
+				$('#die-'+die.id).text(die.val);
 			}
 		},
 	}
@@ -78,25 +78,41 @@ $('document').ready(function(){
 			return model.dice;
 		},
 		clickTransition : {
-			'fresh' : 'held',
-			'held' : 'fresh'
+			'fresh' : 'selected',
+			'selected' : 'fresh',
+			'locked':'locked'
 		},
 		connectViewToModel: function(){
-
-			console.log(this.clickTransition);
 			$(document).delegate(".die", "click", function(){
 				var die;
-				console.log(this);
 				die = model.dice[$(this).attr('id').slice(-1)];
-				console.log('this is '+$(this).html());
 				$(this).removeClass('die-'+die.state);
 				die.state = octopus.clickTransition[die.state];
 				$(this).addClass('die-'+die.state);
-				// $(this).attr('class','die die-'+die.state);
-				// $(this).attr('id', 'die-'+die.id);
-				// $(this).text(die.val);
-
+				$(this).text(die.val);
 			});
+			$('#roll').click(function(){
+				var die;
+				for (var i=0; i<model.dice.length; i++){
+					die = model.dice[i];
+					if (die.state == 'selected'){
+						die.state = 'locked';
+					}
+					if (die.state == 'fresh')
+						die.val = Math.floor(Math.random() * 6) + 1;
+				}
+				view.render();
+			});
+			$('#clear').click(function(){
+				var die;
+				for (var i=0; i<model.dice.length; i++){
+					die = model.dice[i];
+					if (die.state == 'selected'){
+						die.state = 'fresh';
+					}
+				}
+				view.render();
+			})
 		}
 
 	}
@@ -108,27 +124,5 @@ $('document').ready(function(){
 	model.initialize();
 	view.initialize();
 	octopus.connectViewToModel();
-	// setInterval(function(){
-	// 	update()
-	// }, 500)
-
-	// rollTransition = {
-	// 	'held' : roll
-	// };
-	// $('div.die').click(function(){
-	// 	var die;
-	// 	die = model.dice[$(this).attr('id').slice(-1)];
-	// 	console.log('this is '+this);
-	// 	die.state = clickTransition[die.state];
-	// 	$(this).attr('class','die die-'+die.state);
-	// });
-	// $('#roll').click(function(){
-	// 	for (var i=0; i<model.dice.length; i++){
-	// 		var die;
-	// 		die = model.dice[i];
-	// 		die.state = rollTransition[die.state];
-	// 		$(this).attr('class','die die-'+die.state);
-	// 	}
-	// });
 });
 
